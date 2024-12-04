9b93318c146d2bce8df1f0c020219254c8818a6c
sachin jaiswal <jayswalsachin012@gmail.com>
2024-12-05 02:05:17 +0530

    parameter constructor

---
+++ OOP/parametrisedConstructor.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Chai {
+     public: //Access Modifier
+
+        //Data memebrs (attr)
+        string teaName;
+        int servings;
+
+        vector<string> ingredients; // List of ingredient for tea.
+
+        // parameterised Constructor
+        Chai(string name,int serv,vector<string>ingr){
+            teaName = name;
+            servings = serv;
+            ingredients = ingr;
+            cout<<"Parameter Constructor called!"<<endl;
+        }
+
+        //memebr functions
+
+        void displayChaiDetails(){
+            cout<<"Tea Name :" <<teaName<<endl;
+            cout<<"Servings :" <<servings<<endl;
+            cout<<"Ingredients: ";
+            //Loop
+            for(string ingridient : ingredients){
+                cout<<ingridient<<" ";
+            }
+            cout<<endl;
+        }
+};
+
+
+int main(){
+    Chai lemonChai("Lemon tea",1,{"Water,","Tea,","Honey"}); // Default constructor invoked when we create object.
+
+    lemonChai.displayChaiDetails();
+}
