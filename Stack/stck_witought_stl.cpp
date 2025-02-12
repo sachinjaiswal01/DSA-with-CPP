c9bb0af05d80c41485c74d8195cd4ea7bfb4b0c0
sachinjaiswal01 <jayswalsachin012@gmail.com>
2025-02-12 16:22:18 +0530

    ...

--- Stack/stck_witought_stl.cpp
+++ Stack/stck_witought_stl.cpp
@@ -2,6 +2,26 @@
 
 class stack{
     //Properties
+    public:
+    int *arr;
+    int top;
+    int size;
 
     //behaviour
+    //constructor
+    stack(int size){
+        this -> size = size;
+        arr = new int[size]; // new array
+        top = -1;
+    }
+    void push(){
+
+    }
+    void pop(){
+        
+    }
+    void peek(){
+        
+    }
+
 };
